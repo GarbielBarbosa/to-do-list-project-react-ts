f0f5ad8f9630a15cc922409892ea11182d20ff9b
Gabriel Barbosa <gabriel1815@gmail.com>
2023-02-28 17:07:31 -0300

    Atualiza Title e logo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Header } from "./components/Header";
 import styles from "./App.module.css";
 import "./global.css";
+import { ToDo } from "./components/To-do";
 
 
 function App() {
   return (
     <div>
       <Header />
+      <ToDo />
     </div>
   );
 }
